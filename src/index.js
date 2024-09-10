a36a201f438958794e6c6ccb71259fcd3ec95dee
Phan Dat <dat66751@gmail.com>
2024-09-10 09:38:55 +0700

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const morgan = require("morgan");
+const handlebars = require("express-handlebars");
+
+const app = express();
+const port = 3000;
+
+//HTTP logger
+app.use(morgan("combined"));
+
+//Template engine
+app.engine("handlebars", handlebars);
+app.set("view engine", "handlebars");
+
+app.get("/", function (req, res) {
+  res.render("home");
+});
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
