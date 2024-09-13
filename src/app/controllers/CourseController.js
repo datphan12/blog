5a9df47f2fea95cf32e1951816ba9e45cd3e17f1
Phan Dat <dat66751@gmail.com>
2024-09-13 13:58:19 +0700

    add page create course

--- src/app/controllers/CourseController.js
+++ src/app/controllers/CourseController.js
@@ -8,6 +8,26 @@ class CourseController {
       .then((course) => res.render("courses/show", { course: mongooseToObject(course) }))
       .catch(next);
   }
+
+  //[GET] /courses/create
+  create(req, res, next) {
+    res.render("courses/create");
+  }
+  //[POST] /courses/store
+  async store(req, res, next) {
+    try {
+      const formData = req.body;
+      formData.image = `https://i.ytimg.com/vi/${req.body.videoId}/hqdefault.jpg?sqp=-oaymwEbCKgBEF5IVfKriqkDDggBFQAAiEIYAXABwAEG&rs=AOn4CLCML-byK5TPhWN_-ZuZal4h5KasYw`;
+
+      const course = new Course(formData);
+      await course.save();
+
+      res.redirect("/");
+    } catch (err) {
+      console.log(err);
+      next(err);
+    }
+  }
 }
 
 module.exports = new CourseController();
